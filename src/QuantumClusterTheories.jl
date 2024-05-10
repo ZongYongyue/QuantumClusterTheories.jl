fff39abb20e942250eaeb85f42320bf90ca22162
ZongYongyue <zongyyphy@qq.com>
2024-05-10 16:07:06 +0800

    add SVCA for spliced lattices

--- src/QuantumClusterTheories.jl
+++ src/QuantumClusterTheories.jl
@@ -59,7 +59,7 @@ function Clusteration(hilbert::Hilbert, bs::BinaryBases; cid::Union{Nothing, Abs
 end
 
 """
-    VCA{L<:AbstractLattice, G<:EDSolver, P<:Perioder} <: Frontend
+    VCA{L<:AbstractLattice, S<:AbstractLattice, G<:EDSolver, P<:Perioder, T<:Partition} <: Frontend
 
 Variational Cluster Approach(VCA) method for a quantum lattice.
 """
@@ -82,6 +82,7 @@ function Base.repr(gen::OperatorGenerator; ndecimal::Int=6)
     end
     return @sprintf "%s" join(result, "")
 end
+
 """
     VCA(sym::Symbol, unitcell::AbstractLattice, cluster::AbstractLattice, hilbert::Hilbert, origiterms::Tuple{Vararg{Term}}, referterms::Tuple{Vararg{Term}}, bs::BinaryBases; neighbors::Union{Nothing, Int, Neighbors}=nothing, m::Int=200)
 
@@ -113,6 +114,7 @@ function VCA(sym::Symbol, unitcell::AbstractLattice, cluster::AbstractLattice, h
     end
     return VCA(modelname, cachepath, unitcell, cluster, origigenerator, refergenerator, edsolver, perioder, parts)
 end
+
 function VCA(sym::Symbol, unitcell::AbstractLattice, cluster::AbstractLattice, clusteration::Clusteration, origiterms::Tuple{Vararg{Term}}; perioder::Union{Perioder, Nothing}=nothing, modelname::Union{Nothing,String}=nothing, cachepath::Union{Nothing, String}=nothing)
     hilbert, bs, neighbors, referterms, m = clusteration.hilbert, clusteration.bs, clusteration.neighbors, clusteration.referterms, clusteration.m
     return VCA(sym, unitcell, cluster, hilbert, origiterms, referterms, bs; perioder=perioder, neighbors=neighbors, m=m, modelname=modelname, cachepath=cachepath)
@@ -141,6 +143,105 @@ function update!(vca::VCA, oparams::Parameters, rparams::Parameters)
     end      
     return vca
 end
+
+"""
+    SVCA{U<:AbstractLattice, L<:AbstractLattice, C<:AbstractVector{L}, A<:AbstractLattice, G<:EDSolver, E<:AbstractVector{G}, P<:Perioder, T<:Partition, R<:AbstractVector{T}} <: Frontend
+
+Variational Cluster Approach(VCA) method for spliced quantum lattices.
+"""
+mutable struct SVCA{U<:AbstractLattice, L<:AbstractLattice, C<:AbstractVector{L}, A<:AbstractLattice, G<:EDSolver, E<:AbstractVector{G}, P<:Perioder, T<:Partition, R<:AbstractVector{T}} <: Frontend
+    const modelname::Union{String, Nothing}
+    const cachepath::Union{String, Nothing}
+    const unitcell::U
+    const clusters::C
+    const lattice::A
+    const origigenerator::OperatorGenerator
+    const refergenerator::OperatorGenerator
+    solvers::E
+    const perioder::P
+    const partses::R
+end
+
+"""
+    SVCA(sym::Symbol, unitcell::AbstractLattice, clusters::AbstractVector{<:AbstractLattice}, clns::AbstractVector{<:Clusteration}, lvectors::AbstractVector, hilbert::Hilbert, origiterms::Tuple{Vararg{Term}}, referterms::Tuple{Vararg{Term}}; perioder::Union{Perioder, Nothing}=nothing, neighbors::Union{Nothing, Int, Neighbors}=nothing, modelname::Union{Nothing,String}=nothing, cachepath::Union{Nothing, String}=nothing)
+
+Construct the Variational Cluster Approach(VCA) method for spliced quantum lattice systems with EDSolver.
+"""
+function SVCA(sym::Symbol, unitcell::AbstractLattice, clusters::AbstractVector{<:AbstractLattice}, clns::AbstractVector{<:Clusteration}, lvectors::AbstractVector, hilbert::Hilbert, origiterms::Tuple{Vararg{Term}}, referterms::Tuple{Vararg{Term}}; perioder::Union{Perioder, Nothing}=nothing, neighbors::Union{Nothing, Int, Neighbors}=nothing, modelname::Union{Nothing,String}=nothing, cachepath::Union{Nothing, String}=nothing)
+    @assert length(clusters)==sum(length(cln.cid) for cln in clns) "The lengths of clusters and clusterations is inconsistent"
+    table = Table(hilbert, Metric(EDKind(hilbert), hilbert))
+    lattice = Lattice(:lattice, hcat([cluster.coordinates for cluster in clusters]...), QuantumLattices.Spatials.vectorconvert(lvectors))
+    splice = vcat([[i for _ in axes(clusters[i].coordinates, 2)] for i in eachindex(clusters)]...)
+    isnothing(neighbors) && (neighbors = maximum(term->term.bondkind, origiterms))
+    origibonds = bonds(lattice, neighbors)
+    referbonds = filter(bondd->isintracell(bondd), filter(bbond->isintraclusters(bbond, splice), origibonds))
+    isnothing(perioder) ? (perioder = Perioder(unitcell, lattice, table)) : (perioder = perioder)
+    origigenerator, refergenerator = OperatorGenerator(origiterms, origibonds, hilbert; table = table), OperatorGenerator(referterms, referbonds, hilbert; table = table)
+    partses = Vector{Partition}(undef, length(clns))
+    edsolvers = Vector{EDSolver}(undef, length(clns))
+    for i in eachindex(clns)
+        ctable = Table(clns[i].hilbert, Metric(EDKind(clns[i].hilbert), clns[i].hilbert))
+        isnothing(clns[i].referterms) ? (creferterms=referterms) : (creferterms=clns[i].referterms)
+        isnothing(clns[i].neighbors) ? (cneighbors=neighbors) : (cneighbors=clns[i].neighbors)
+        partses[i] = Partition(sym, ctable, clns[i].bs)
+        creferbonds = filter(bond -> isintracell(bond), bonds(clusters[clns[i].cid[1]], cneighbors))
+        crefergenerator = OperatorGenerator(creferterms, creferbonds, clns[i].hilbert; table = ctable)
+        if !isnothing(cachepath)
+            modelname_str = isnothing(modelname) ? "default_model" : modelname
+            cache_file_path = joinpath(cachepath, @sprintf "%s_%s_%s_%s.jls" modelname_str clns[i].cid repr(crefergenerator) EDSolver )
+            if isfile(cache_file_path)
+                edsolvers[i] = deserialize(cache_file_path)
+                println("Load edsolver from $cache_file_path")
+            else
+                !isdir(cachepath)&& mkdir(cachepath)
+                println("Cache directory created at $cachepath")
+                edsolvers[i] = EDSolver(EDKind(clns[i].hilbert), partses[i], crefergenerator, clns[i].bs, ctable; m = clns[i].m)
+                serialize(cache_file_path, edsolver)
+                println("EDSolver cached at $cache_file_path")
+            end
+        else
+            edsolvers[i] = EDSolver(EDKind(clns[i].hilbert), partses[i], crefergenerator, clns[i].bs, ctable; m = clns[i].m)
+        end
+    end
+    return SVCA(modelname, cachepath, unitcell, clusters, lattice, origigenerator, refergenerator, edsolvers, perioder, partses)
+end
+
+isintraclusters(bond::Bond, splice::AbstractVector{<:Integer}) = all(point-> splice[bond.points[1].site]==splice[point.site] , bond)
+
+function update!(svca::SVCA, oparams::Parameters, rparams::Parameters, clns::AbstractVector{<:Clusteration})
+    update!(svca.origigenerator; oparams...)
+    update!(svca.refergenerator; rparams...)
+    cachepath = svca.cachepath
+    modelname = svca.modelname
+    edsolvers = Vector{EDSolver}(undef, length(clns))
+    for i in eachindex(clns)
+        ctable = Table(clns[i].hilbert, Metric(EDKind(clns[i].hilbert), clns[i].hilbert))
+        creferterms=clns[i].referterms
+        isnothing(clns[i].neighbors) ? (cneighbors = maximum(term->term.bondkind, creferterms)) : (cneighbors=clns[i].neighbors)
+        creferbonds = filter(bond -> isintracell(bond), bonds(svca.clusters[clns[i].cid[1]], cneighbors))
+        crefergenerator = OperatorGenerator(creferterms, creferbonds, clns[i].hilbert; table = ctable)
+        if !isnothing(cachepath)
+            modelname_str = isnothing(modelname) ? "default_model" : modelname
+            cache_file_path = joinpath(cachepath, @sprintf "%s_%s_%s_%s.jls" modelname_str clns[i].cid repr(crefergenerator) EDSolver)
+            if isfile(cache_file_path)
+                edsolvers[i] = deserialize(cache_file_path)
+                println("Load edsolver from $cache_file_path")
+            else
+                !isdir(cachepath)&& mkdir(cachepath)
+                println("Cache directory created at $cachepath")
+                edsolvers[i] = EDSolver(EDKind(clns[i].hilbert), svca.partses[i], crefergenerator, svca.partses[i].sector, ctable; m = clns[i].m)
+                serialize(cache_file_path, edsolver)
+                println("EDSolver cached at $cache_file_path")
+            end
+        else
+            edsolvers[i] = EDSolver(EDKind(clns[i].hilbert), svca.partses[i], crefergenerator, svca.partses[i].sector, ctable; m = clns[i].m)
+        end
+    end
+    svca.solvers = edsolvers
+    return svca
+end
+
+
 """
     origiQuadraticTerms!(normal::Bool, om::AbstractMatrix, oops::AbstractVector, oopsseqs::AbstractVector,k::AbstractVector)
 
